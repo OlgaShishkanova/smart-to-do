e76aadda739651ec61a18a7e7b97dfb3f2fe47f5
Olga <lili432@mail.ru>
2024-03-07 16:32:52 +0100

    delete functionality

--- components/ToDo/ToDoItem.tsx
+++ components/ToDo/ToDoItem.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Animated, StyleSheet, TextInput } from "react-native";
+import { Animated, StyleSheet, Text, TextInput } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import Checkbox from "expo-checkbox";
 
 import { View } from "@/components/Themed";
@@ -8,16 +9,23 @@ import { COLORS } from "@/constants/Colors";
 import { useToDoStore } from "@/store";
 import React from "react";
 import { useFadeAnimation } from "@/hooks/useFadeAnimation";
+import {
+  Menu,
+  MenuOption,
+  MenuOptions,
+  MenuTrigger,
+} from "react-native-popup-menu";
 
 interface Props {
   item: ToDoItemType;
 }
 
 const ToDoItem = ({ item }: Props) => {
-  const { editToDoItem } = useToDoStore((state) => state);
+  const { editToDoItem, removeToDoItems } = useToDoStore((state) => state);
   const isToDoOpen = item.status === ToDoStatus.Open;
   const [inputText, changeInputText] = useState(item.text);
   const [isChecked, setChecked] = useState(!isToDoOpen);
+  const [isMenu, setIsMenu] = useState(false);
   const { fadeAnim, fadeOut } = useFadeAnimation();
   const onEditFinished = () => {
     editToDoItem(item.id, { text: inputText, editDate: new Date() });
@@ -30,6 +38,12 @@ const ToDoItem = ({ item }: Props) => {
         status: value ? ToDoStatus.Done : ToDoStatus.Open,
       });
     }, 1000);
+  };
+  const onOptionSelect = (value: string) => {
+    //TODO: make it as enum + more options
+    if (value === "delete") {
+      removeToDoItems([item.id]);
+    }
   };
   return (
     <View>
@@ -47,14 +61,38 @@ const ToDoItem = ({ item }: Props) => {
           onValueChange={(value) => onChecked(value)}
           color={isChecked ? COLORS.tertiary : undefined}
         />
-        <TextInput
-          multiline
-          editable={isToDoOpen}
-          onChangeText={(text) => changeInputText(text)}
-          onBlur={onEditFinished}
-          value={inputText}
-          style={{ padding: 10, width: "100%" }}
-        />
+        {isToDoOpen ? (
+          <TextInput
+            multiline
+            onChangeText={(text) => changeInputText(text)}
+            onBlur={onEditFinished}
+            value={inputText}
+            style={{ padding: 10, width: "90%" }}
+          />
+        ) : (
+          <View style={styles.menuWrapper}>
+            <Text disabled style={styles.text}>
+              {inputText}
+            </Text>
+            <Menu
+              opened={isMenu}
+              onBackdropPress={() => setIsMenu(false)}
+              onSelect={(value) => onOptionSelect(value)}
+            >
+              <MenuTrigger
+                style={styles.iconWrapper}
+                onPress={() => setIsMenu(true)}
+              >
+                <Ionicons size={18} color="black" name="ellipsis-vertical" />
+              </MenuTrigger>
+              <MenuOptions>
+                <MenuOption value="delete">
+                  <Text>Delete</Text>
+                </MenuOption>
+              </MenuOptions>
+            </Menu>
+          </View>
+        )}
       </Animated.View>
     </View>
   );
@@ -70,11 +108,21 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
   },
-  paragraph: {
-    fontSize: 15,
-  },
   checkbox: {
     margin: 8,
+  },
+  text: {
+    textDecorationLine: "line-through",
+    width: "90%",
+  },
+  menuWrapper: {
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    width: "90%",
+  },
+  iconWrapper: {
+    padding: 10,
   },
 });
 export default React.memo(ToDoItem);
